fb5c48bf0684491b0799759ff53a428d9f77826b
Jonathan Ohana <jons1122@gmail.com>
2021-11-28 20:53:45 +0200

    Created new classes
    Created Student and Consultant classes that inherit from User.
    
    Created Institute class that represents
    an educational institute

---
+++ ConsultantUser.py
@@ -0,0 +1,5 @@
+from User import User
+
+class ConsultantUser(User):
+    def __init__(self):
+        User.__init__(self)
---
+++ Institute.py
@@ -0,0 +1,5 @@
+class Institute():
+    def __init__(self, institute_name, institute_id, student_list):
+        self.institute_name = institute_name
+        self.institute_id = institute_id
+        self.student_list = student_list
