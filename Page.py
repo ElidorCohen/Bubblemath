b9047b440bc44ce4ad19a7b1bc299e16a819b1d2
YosiKariv1 <93153387+YosiKariv1@users.noreply.github.com>
2021-11-28 20:37:26 +0200

    רקע
    הוספת רקע למסך שניפתח

--- Page.py
+++ Page.py
@@ -1,29 +1,28 @@
 import pygame
 
 class Page():
+    pygame.display.set_caption("BubbleMath")
     def __init__(self):
-        self.PAGE_W = 640
-        self.PAGE_H = 480
+        self.PAGE_W = 1000
+        self.PAGE_H = 720
         self.page_id = 0
         self.background_colour = (255, 207, 132)
         self.run_page = True
-        self.window = pygame.display.set_mode((self.PAGE_W, self.PAGE_H))
+        self.display = pygame.Surface((self.PAGE_W, self.PAGE_H))
+        self.window = pygame.display.set_mode(((self.PAGE_W, self.PAGE_H)))
         self.clock = pygame.time.Clock()
-
-    def blit_screen(self):
-        self.display.fill(self.background_colour)
-        pygame.display.flip()
-        pygame.display.update()
-        pygame.display.set_caption("BubbleMath")
 
 
     def display_menu(self):
         self.run_page = True
         while self.run_page:
+            self.window.fill(self.background_colour)
             self.clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:  # Check if Rage Quit
                     self.run_page = False
+            pygame.display.update()
+
 
 
 
