97747650579b7f21b00e108b5c9fc9eeffe25380
barferera <barferera@gmail.com>
2021-11-28 19:02:40 +0200

    Create User.py
    created user class

---
+++ User.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class UserType (Enum):
+    counselor = 1
+    student = 2
+
+
+class Gender (Enum):
+    Male = 1
+    Female = 2
+
+
+class User:
+    def __init__(self, user_id, first_name, last_name, age,user_type, gender):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.user_type = user_type
+        self.gender = gender
+        self.user_type = user_type
